b58049c4106c305c5137b34588dc9528a872a49b
shehla <13743@holbertonstudents.com>
2026-05-23 06:37:40 +0000

    task 3

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cdr=$1
-printf "%d.%d.%d.%d\n" "$(( (0xFFFFFFFF << (32 - cdr)) >> 24 & 0xFF ))" "$(( (0xFFFFFFFF << (32 - cdr)) >> 16 & 0xFF ))" "$(( (0xFFFFFFFF << (32 - cdr)) >> 8 & 0xFF ))" "$(( (0xFFFFFFFF << (32 - cdr)) & 0xFF ))"
+printf "%d.%d.%d.%d\n" "$(( (0xFFFFFFFF << (32 - cdr)) >> 24 & 0xFF ))" "$(( (0xFFFFFFFF << (32 - cdr)) >> 16 & 0xFF ))" "$(( (0xFFFFFFFF << (32 - cdr)) >> 8 & 0xFF ))" "$(( (0xFFFFFFFF << (32 - cdr)) & 0xFF))"
